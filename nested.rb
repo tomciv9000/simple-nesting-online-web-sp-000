e193319426fc1adf380e1fcfe603fa44f3a93623
Tom White <eugene.white@gmail.com>
2019-05-27 03:43:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  
+}
+]
     
 end
 
